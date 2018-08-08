3e9248f49478d4d4ed43e0b51a363f0ad55e057f
Sarra_ELGHALI <s.elghali@niryo.com>
2018-08-08 09:40:53 +0200

    applay white yellow masks in hls_converted image

--- self_driving_car_modules/line_extraction.py
+++ self_driving_car_modules/line_extraction.py
@@ -52,3 +52,17 @@ def convert_hsv(image):
 
 def convert_hls(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+
+def select_white_yellow(image):
+    converted = convert_hls(image)
+    # white color mask
+    lower = np.uint8([  0, 200,   0])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(converted, lower, upper)
+    # yellow color mask
+    lower = np.uint8([ 10,   0, 100])
+    upper = np.uint8([ 40, 255, 255])
+    yellow_mask = cv2.inRange(converted, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    return cv2.bitwise_and(image, image, mask = mask)
