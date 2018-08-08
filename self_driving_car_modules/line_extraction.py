9d56bb7c0af3fbf2b88bab7b99808eb2cc460299
Sarra_ELGHALI <s.elghali@niryo.com>
2018-08-08 09:47:33 +0200

    Averaging and Extrapolating Lines

--- self_driving_car_modules/line_extraction.py
+++ self_driving_car_modules/line_extraction.py
@@ -126,3 +126,72 @@ def draw_lines(image, lines, color=[255, 0, 0], thickness=2, make_copy=True):
         for x1,y1,x2,y2 in line:
             cv2.line(image, (x1, y1), (x2, y2), color, thickness)
     return image
+
+# Averaging and Extrapolating Lines
+def average_slope_intercept(lines):
+    left_lines    = [] # (slope, intercept)
+    left_weights  = [] # (length,)
+    right_lines   = [] # (slope, intercept)
+    right_weights = [] # (length,)
+    
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x2==x1:
+                continue # ignore a vertical line
+            slope = (y2-y1)/(x2-x1)
+            intercept = y1 - slope*x1
+            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+            if slope < 0: # y is reversed in image
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    
+    # add more weight to longer lines    
+    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+    
+    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+
+def make_line_points(y1, y2, line):
+    """
+    Convert a line represented in slope and intercept into pixel points
+    """
+    if line is None:
+        return None
+    
+    slope, intercept = line
+    
+    # make sure everything is integer as cv2.line requires it
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    
+    return ((x1, y1), (x2, y2))
+
+
+def lane_lines(image, lines):
+    left_lane, right_lane = average_slope_intercept(lines)
+    
+    y1 = image.shape[0] # bottom of the image
+    y2 = y1*0.6         # slightly lower than the middle
+
+    left_line  = make_line_points(y1, y2, left_lane)
+    right_line = make_line_points(y1, y2, right_lane)
+    
+    return left_line, right_line
+
+    
+def draw_lane_lines(image, lines, color=[255, 0, 0], thickness=20):
+    # make a separate image to draw lines and combine with the orignal later
+    line_image = np.zeros_like(image)
+    for line in lines:
+        if line is not None:
+            cv2.line(line_image, *line,  color, thickness)
+    # image1 * α + image2 * β + λ
+    # image1 and image2 must be the same shape.
+    return cv2.addWeighted(image, 1.0, line_image, 0.95, 0.0)
+             
+    
