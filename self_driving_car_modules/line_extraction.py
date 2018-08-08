c73b26ae8dc1e26f2092ddd42e7d8da9910210d1
Sarra_ELGHALI <s.elghali@niryo.com>
2018-08-08 09:36:06 +0200

    add 3 different methodes to select colors

--- self_driving_car_modules/line_extraction.py
+++ self_driving_car_modules/line_extraction.py
@@ -29,3 +29,26 @@ def show_images(images, cmap=None):
         plt.yticks([])
     plt.tight_layout(pad=0, h_pad=0, w_pad=0)
     plt.show() 
+
+
+# clor selection 
+# image is expected be in RGB color space
+def select_rgb_white_yellow(image): 
+    # white color mask
+    lower = np.uint8([200, 200, 200])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(image, lower, upper)
+    # yellow color mask
+    lower = np.uint8([190, 190,   0])
+    upper = np.uint8([255, 255, 255])
+    yellow_mask = cv2.inRange(image, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    masked = cv2.bitwise_and(image, image, mask = mask)
+    return masked
+def convert_hsv(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
+
+def convert_hls(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
