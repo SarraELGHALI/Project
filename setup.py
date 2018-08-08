b3c9a5e1cbc908df1bfca71e79c5cdf31c550e3f
Sarra_ELGHALI <s.elghali@niryo.com>
2018-08-08 10:33:15 +0200

    setup compilation env

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='self_driving_car_modlues',
+      version='0.1',
+      description='self dirving car modules',
+      author='Sarra Elghali',
+      author_email='sarra.elghali@gmail.com',
+      packages=['self_driving_car_modules']
+     )
